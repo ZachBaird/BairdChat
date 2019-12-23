2190da6badbaf3acdb6508ceab86d16a61dc87ff
Zach Baird <bairdz@leidos.com>
2019-12-23 15:54:19 -0500

    Init commit

---
+++ SignalRDemo/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRDemo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "Server", "Someone has entered the chat...");
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "Server", "Someone has dropped from the chat...");
+        }
+    }
+}
---
+++ SignalRDemo/wwwroot/js/chat.js
@@ -0,0 +1,55 @@
+﻿"use strict";
+
+// ------------------------------------------------------------------------------------------------
+// On page load actions.
+// ------------------------------------
+
+// Initialize hub connection with mapped URL from Startup.cs.
+const connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+// Disable send button until connection is established.
+document.getElementById("sendButton").disabled = true;
+// ------------------------------------------------------------------------------------------------
+
+
+
+//-------------------------------------------------------------------------------------------------
+// Declare events where client receives action from server.
+//-------------------------------------
+
+// When the client receives a message, append the message to the ul#messagesList.
+connection.on("ReceiveMessage", (user, message) => {
+    const msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    const encodedMsg = user + ": " + msg;
+    const li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+//-------------------------------------------------------------------------------------------------
+
+
+
+// Start the connection.
+connection.start().then(() => {
+    document.getElementById("sendButton").disabled = false;
+}).catch(err => {
+    return console.error(err.toString());
+});
+
+
+
+//-------------------------------------------------------------------------------------------------
+// Declare events the client starts that will be sent up to the server.
+//-------------------------------------
+
+// Send a message to the server.
+document.getElementById("sendButton").addEventListener("click", e => {
+    const user = document.getElementById("userInput").value;
+    const message = document.getElementById("messageInput").value;
+
+    // Call .invoke on "connection" to send up event to server hub.
+    connection.invoke("SendMessage", user, message).catch(err => {
+        return console.error(err.toString());
+    })
+    e.preventDefault();
+});
+//-------------------------------------------------------------------------------------------------
